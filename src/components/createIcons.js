ba7d2748a069412ed598f07de6996fb5713ff80d
0x5b3a <101mailme@protonmail.com>
2024-05-22 16:00:59 +0530

    Created new development branch\nDeleted temporary files\ndeleted files which are no longer in use and can be a security leak\nfixed some typos & style\nfixed footer links\n Icons creation(js) are now in it's seprate file

---
+++ src/components/createIcons.js
@@ -0,0 +1,32 @@
+import { createIcons, Clock,CalendarRange, BookOpenCheck, Handshake, Users, Video, MessagesSquare, FileBadge, CalendarClock, Target, HandCoins, ReceiptIndianRupee, IndianRupee, LockOpen, Linkedin, Github, Instagram, Mail, Building2, Rocket, PencilRuler, Twitter, } from "lucide"
+
+export const createLucideIcons = createIcons({
+    icons: {
+        Clock,
+        CalendarRange,
+        BookOpenCheck,
+        Handshake,
+        Users,
+        Video,
+        MessagesSquare,
+        FileBadge,
+        CalendarClock,
+        PencilRuler,
+        HandCoins,
+        ReceiptIndianRupee,
+        IndianRupee,
+        LockOpen,
+        Linkedin,
+        Github,
+        Instagram,
+        Mail,
+        Building2,
+        Rocket,
+        Twitter
+    },
+    attrs:{
+        class: ["icons","lucide"],
+        height: "",
+        width: "",
+    }
+})
