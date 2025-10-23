677c1d2e7dae419447cb28c88e10bd41dc261622
Samiuddin <mdsamiuddin2005@outlook.com>
2025-10-23 21:31:54 +0530

    feat : add pyweek page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
-
+import path from 'path';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [react(), tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('./src')
+      }
+    }
+  }
 });
